947504d0b1af11e8bc8e028a4fd32909155531dd
Zhang Dongliang <zhangdongliang@yunion.cn>
2021-08-23 17:23:27 +0800

    fix(db,k8s,backup,restore): k8s 的备份与恢复，修复sync-user报错的问题

--- lib/db.py
+++ lib/db.py
@@ -1,12 +1,12 @@
-import MySQLdb
 import os
-
-from cmd import run_bash_cmd, archive_with_pv
+import MySQLdb
 from shutil import copyfile
+
+from cmd import run_bash_cmd
 from .utils import print_title
 
-PV_ARGS = 'pv --timer --eta'
-
+PV_ARGS = 'pv --timer --rate --eta'
+MYSQL_BACKUP_ARGS = '--add-drop-database --add-drop-table --add-locks --single-transaction --quick '
 class DB():
     def __init__(self, config=None, database="", host='127.0.0.1', user='root', passwd=None, port=3306):
         values = {
@@ -100,48 +100,51 @@ def backup_db_table(config, db, table):
     _db = DB(config)
     db_size = _db.get_db_size([db], [table])
     db_args = gen_db_config_args(config)
+    mysql_backup_args = MYSQL_BACKUP_ARGS
 
-    cmd = 'mysqldump --hex-blob --single-transaction --quick --databases %(db)s %(db_args)s --tables %(table)s ' % locals()  # + ' --result-file ' + fn
+    cmd = 'mysqldump %(mysql_backup_args)s --databases %(db)s %(db_args)s --tables %(table)s ' % locals()  # + ' --result-file ' + fn
     cmd += ' | %(PV_ARGS)s --name " Dumping %(name)s" --size %(db_size)s' % {
         'PV_ARGS': PV_ARGS,
         'db_size': db_size,
         'name': db + '.' + table,
     }
-    cmd += ' > %(fn)s' % locals()
+    cmd += '| gzip -c > %(tgz)s' % locals()
     run_bash_cmd(cmd)
-    archive_with_pv(fn, tgz)
-    run_bash_cmd('rm -f %s' % fn)
 
-def backup_db(config, backup_path):
+
+def backup_db(config, backup_path, light=False):
     print_title('back up db...')
     os.chdir(backup_path)
-    backup_db_table(config, 'yunionmeter', 'bills_tbl')
-    backup_db_table(config, 'notify', 'tasks_tbl')
     ignores = ''
     dbs = DB(config)
-    for dbname in dbs.get_databases():
-        db = DB(config, dbname)
-        ignores += ' ' + db.gen_export_args()
+    mysql_backup_args = MYSQL_BACKUP_ARGS
+
+    if light is True:
+        backup_db_table(config, 'yunionmeter', 'bills_tbl')
+        backup_db_table(config, 'notify', 'tasks_tbl')
+        for dbname in dbs.get_databases():
+            db = DB(config, dbname)
+            ignores += ' ' + db.gen_export_args()
+
+        ignores += ' --ignore-table=yunionmeter.bills_tbl '
+        ignores += ' --ignore-table=notify.tasks_tbl '
+
     db_name_str = ' '.join(dbs.get_databases())
 
-    ignores += ' --ignore-table=yunionmeter.bills_tbl '
-    ignores += ' --ignore-table=notify.tasks_tbl '
-
-    fn = '%s/onecloud.sql' % backup_path
-    cmd = 'mysqldump --hex-blob --single-transaction --quick --databases ' + db_name_str + ' ' + gen_db_config_args(config) + ignores
+    tgz = '%s/onecloud.sql.tgz' % backup_path
+    cmd = 'mysqldump %(mysql_backup_args)s --databases ' % locals() + db_name_str + ' ' \
+        + gen_db_config_args(config) + ignores
 
     db_size = dbs.get_db_size(dbs.get_databases(), ignores=ignores)
-    cmd += ' | %(PV_ARGS)s --name " Dumping %(name)s" --size %(db_size)s' % {
+    cmd += ' | %(PV_ARGS)s --name " Dumping Database" --size %(db_size)s' % {
         'PV_ARGS': PV_ARGS,
         'db_size': db_size,
-        'name': fn,
     }
-    cmd += ' > %s' % fn
+    cmd += ' | gzip -c > %s' % tgz
 
     run_bash_cmd(cmd)
-    archive_with_pv('onecloud.sql', 'onecloud.sql.tgz')
-    run_bash_cmd('rm -f onecloud.sql')
     run_bash_cmd('ls -lah %s/*tgz' % backup_path)
+
 
 def backup_config(src, dest):
     print_title('backing up the config')
