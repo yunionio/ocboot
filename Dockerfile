4d3177dd67bd5e667c9c8df3323564bf8fb00919
yu fei <zyfjqk@163.com>
2021-12-27 16:12:32 +0800

    Ocboot is added to the container image

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,11 @@
 FROM python:3.9-alpine3.13
 ARG ANSIBLE_VERSION=2.9.25
-# install tools
-RUN apk add --no-cache openssh openssl
-# install build_dependencies
+RUN apk add --no-cache openssh openssl curl rsync
 RUN apk add --no-cache --virtual .build-dependencies \
-    libffi-dev openssl-dev python3-dev build-base py-setuptools rust cargo && \
+    libffi-dev openssl-dev python3-dev build-base py-setuptools rust cargo git && \
     pip install --no-cache-dir https://releases.ansible.com/ansible/ansible-${ANSIBLE_VERSION}.tar.gz && \
     pip install paramiko && \
+    git clone https://github.com/yunionio/ocboot.git /opt/ocboot && \
     apk del --no-network .build-dependencies && \
     rm -rf /root/.cache /root/.cargo
 WORKDIR /opt/ocboot
--- run-in-docker.sh
+++ run-in-docker.sh
@@ -1,46 +1,25 @@
 #!/bin/bash
 
-: ${OCBOOT_IMAGE="registry.cn-beijing.aliyuncs.com/yunionio/ocboot:v1.0"}
-
-if [ $# -eq 0 ]; then
-  echo "Error: no argument"
-  exit 1
-fi
-
-if [ `grep -c ID=ubuntu /etc/os-release` -ne '0' ];then
-  pkg_manager=apt-get
-elif [ `grep -c 'ID="centos"' /etc/os-release` -ne '0' ];then
-  pkg_manager=yum
-else
-  echo "Error: OS unsupported"
-  exit 2
-fi
-
-if ! git version > /dev/null 2>&1; then
-  echo "Git not installed, trying to install"
-  sudo $pkg_manager -y install git
-fi
-
-if [ ! -d "./ocboot" ];then
-  echo "Ocboot does not exist, Clone ocboot"
-  git clone https://github.com/yunionio/ocboot.git > /dev/null 2>&1
-fi
+: ${OCBOOT_IMAGE="registry.cn-beijing.aliyuncs.com/yunionio/ocboot:latest"}
 
 if ! docker ps > /dev/null 2>&1; then
   echo "Error: Docker unavailable, Please resolve the problem and try again"
   exit 3
 fi
 
-if [ $# -gt 1 ]; then
+if [ $# -ge 2 ]; then
   echo 'copy config file to "ocboot" directory'
   for i in $@ ; do
     if [ -f $i ]; then
-      cp -uv $i ocboot
+      CONF=$i
     fi
   done
-  docker run -t --network host -v ~/.ssh/id_rsa:/root/.ssh/id_rsa -v `pwd`/ocboot:/opt/ocboot/ $OCBOOT_IMAGE $@
+  docker run -t --network host -v ~/.ssh/id_rsa:/root/.ssh/id_rsa -v `pwd`/$CONF:/opt/ocboot/$CONF $OCBOOT_IMAGE $@
+elif [ $# -eq 1 ]; then
+  docker run -t --network host -v ~/.ssh/id_rsa:/root/.ssh/id_rsa --entrypoint /opt/ocboot/run.py $OCBOOT_IMAGE $@
 else
-  docker run -t --network host -v ~/.ssh/id_rsa:/root/.ssh/id_rsa -v `pwd`/ocboot:/opt/ocboot/ --entrypoint /opt/ocboot/run.py $OCBOOT_IMAGE $@
+  echo "Error: no argument"
+  exit 1
 fi
 
 if [ $? -eq 0 ];then
