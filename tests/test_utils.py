1fa8662c40933e6b93391d0f6aee00221b27109f
Zexi Li <zexi.li@icloud.com>
2022-07-25 19:04:52 +0800

    fix: yunionio image repository when version below v3.9

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -10,3 +10,11 @@ class TestUtils(unittest.TestCase):
         self.assertEqual(time.timestamp(), 1612351985.0)
         time2 = utils.parse_k8s_time("2021-02-03T11:34:05Z")
         self.assertGreater(time2, time)
+
+    def test_is_below_v3_9(self):
+        self.assertTrue(utils.is_below_v3_9('v3.8.11'))
+        self.assertTrue(utils.is_below_v3_9('v3.7.11'))
+        self.assertTrue(utils.is_below_v3_9('v3.6.11'))
+        self.assertTrue(utils.is_below_v3_9('v2.6.11'))
+        self.assertFalse(utils.is_below_v3_9('v3.9.0'))
+        self.assertFalse(utils.is_below_v3_9('master-test'))
