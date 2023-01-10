5bd63b38ce2dfb3677aed6acea585416e2643588
Zhang Dongliang <zhangdongliang@yunion.cn>
2023-01-10 16:18:33 +0800

    fix(backup): 备份：更新备份文件名、readme，增加精简备份模式

--- lib/backup.py
+++ lib/backup.py
@@ -10,7 +10,7 @@ def add_command(subparsers):
         "backup", help="backup onecloud cluster")
     parser.add_argument('config', help="config yaml file")
     parser.add_argument('--backup-path', help="backup path, default: /opt/backup", default="/opt/backup")
-    parser.add_argument('--light', action='store_true', help="ignore opslog_tbl and split yunionmeter, notify databases")
+    parser.add_argument('--light', action='store_true', help="ignore yunionmeter and yunionlogger database; ignore tables start with 'opslog' and 'task'.")
     parser.set_defaults(func=do_backup)
 
 
@@ -19,5 +19,5 @@ def do_backup(args):
     config = ocboot.load_config(args.config)
 
     backup_config(args.config, args.backup_path)
-    backup_db(config, args.backup_path)
+    backup_db(config, args.backup_path, args.light)
     print_title('Backup to: %s' % args.backup_path)
