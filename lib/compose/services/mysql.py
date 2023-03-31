b8fbd70d0904c0e63475192faa9f239be7ba0ae8
Zexi Li <zexi.li@icloud.com>
2023-03-31 11:32:03 +0800

    use mariadb 10.5.19

--- lib/compose/services/mysql.py
+++ lib/compose/services/mysql.py
@@ -7,9 +7,9 @@ class MysqlService(ClusterService):
     def __init__(self,
                  root_pwd="your-sql-password",
                  port="3306",
-                 version="5.6",
+                 version="10.5.19",
                  repo="registry.cn-beijing.aliyuncs.com/yunionio"):
-        super(MysqlService, self).__init__("mysql", version, repo=repo)
+        super(MysqlService, self).__init__("mysql", version, repo=repo, image_name="mariadb")
 
         self.password = root_pwd
         self.port = port
@@ -17,9 +17,13 @@ class MysqlService(ClusterService):
         self.add_environment({
             "MYSQL_ROOT_PASSWORD": self.password,
             "MYSQL_TCP_PORT": self.port,
+            "MYSQL_ROOT_HOST": "%",
+            "MARIADB_AUTO_UPGRADE": "true",
+            "MARIADB_DISABLE_UPGRADE_BACKUP": "true",
         })
         self.add_volume(ServiceDataVolume("/var/lib/mysql"))
-        self.set_healthcheck("mysqladmin ping -h 127.0.0.1 -u $$MYSQL_USER --password=$$MYSQL_PASSWORD")
+        # self.set_healthcheck("/usr/local/bin/healthcheck.sh")
+        self.set_healthcheck("mysqladmin ping -h mysql -P 3306 -p$$MYSQL_ROOT_PASSWORD")
 
     def get_port(self):
         return self.port
