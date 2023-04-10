2cd07b6b09ddbabb819dd57745ff7fec628f984e
Zhang Dongliang <zhangdongliang@yunion.cn>
2023-04-10 17:20:39 +0800

    feat(db,backup): 自动备份数据库

--- lib/parser.py
+++ lib/parser.py
@@ -79,3 +79,15 @@ def inject_add_hostagent_options(parser):
     parser.add_argument("--enable-host-on-vm",
                         dest="enable_host_on_vm",
                         action="store_true", default=False)
+
+
+def inject_auto_backup_options(parser):
+    parser.add_argument('config', help="config yaml file", nargs='?')
+    parser.add_argument(
+        '--backup-path', help="backup path, default: /opt/yunion/backup", default="/opt/yunion/backup")
+    parser.add_argument('--light', action='store_true', default=True,
+                        help="ignore yunionmeter and yunionlogger database; ignore tables start with 'opslog' and 'task'.")
+    parser.add_argument('--max-backups', default=10, type=int,
+                        help="how many backups to keep. default: 10")
+    parser.add_argument('--max-disk-percentage', default=75, type=int,
+                        help="the max usage percentage of the disk on which the backup will be done allowed to perform backup. the backup job will not work when the disk's usage is above the threshold. default: 75(%%).")
