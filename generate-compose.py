8afcdaa1bdb101fa3960e13680558a50c62454e9
Zexi Li <zexi.li@icloud.com>
2023-07-05 14:38:56 +0800

    release v3.10.3

--- generate-compose.py
+++ generate-compose.py
@@ -5,7 +5,7 @@ import os
 from lib.compose.manifest import new_oc_manifest
 
 if __name__ == "__main__":
-    version = os.getenv('VERSION', 'v3.10.0')
+    version = os.getenv('VERSION', 'v3.10.3')
 
     m = new_oc_manifest(version)
     print(m.to_yaml())
