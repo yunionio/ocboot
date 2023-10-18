ed1e731e3ca043182547f129af8010d3229d30ef
Zexi Li <zexi.li@icloud.com>
2023-10-18 19:47:47 +0800

    fix(add-node): support add worker node inside vm defaultly

--- lib/add_node.py
+++ lib/add_node.py
@@ -1,8 +1,31 @@
 # encoding: utf-8
 from __future__ import unicode_literals
 
-from .service import AddNodeService
+from .service import AddNodeService, AddNodesConfig
+from .cluster import construct_cluster
+
+
+class AddWorkerNodeService(AddNodeService):
+
+    def inject_options(self, parser):
+        super(AddNodeService, self).inject_options(parser)
+
+    def do_action(self, args):
+        cluster = construct_cluster(
+            args.primary_master_host,
+            args.ssh_user,
+            args.ssh_private_file,
+            args.ssh_port)
+
+        config = AddNodesConfig(cluster,
+                                args.target_node_hosts,
+                                args.ssh_user,
+                                args.ssh_private_file,
+                                args.ssh_port,
+                                args.ssh_node_port,
+                                enable_host_on_vm=True)
+        config.run()
 
 
 def add_command(subparsers):
-    AddNodeService(subparsers, "add-node", "add new node into cluster")
+    AddWorkerNodeService(subparsers, "add-node", "add new node into cluster")
