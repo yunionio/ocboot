0b247878b8dba752ddd92079fd2e2d1630db957d
Zhang Dongliang <zhangdongliang@yunion.cn>
2023-12-27 10:41:10 +0800

    fix(version): update version to v3.10.10

--- generate-compose.py
+++ generate-compose.py
@@ -5,7 +5,7 @@ import os
 from lib.compose.manifest import new_oc_manifest
 
 if __name__ == "__main__":
-    version = os.getenv('VERSION', 'v3.10.9')
+    version = os.getenv('VERSION', 'v3.10.10')
 
     m = new_oc_manifest(version)
     print(m.to_yaml())
