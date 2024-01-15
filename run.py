325315016adc58408089079130c04f315a6b4eb6
Zhang Dongliang <zhangdongliang@yunion.cn>
2024-01-15 10:29:34 +0800

    refactor url 20240115.1029

--- run.py
+++ run.py
@@ -360,6 +360,12 @@ def generate_config(ipv4, produc_stack, dns_list=[]):
     elif re.search(r'\b\d{8}\.\d$', ver):
         yaml_data[ocboot.GROUP_PRIMARY_MASTER_NODE]['image_repository'] = 'registry.cn-beijing.aliyuncs.com/yunionio'
 
+    if '5000' in image_repository:
+        r = image_repository
+        if '/' in r:
+            r = r.split('/')[0]
+        yaml_data[ocboot.GROUP_PRIMARY_MASTER_NODE]['insecure_registries'] = [r]
+
     interface = get_interface_by_ip(ipv4)
     username = get_username()
     db_password = random_password(12) if brand_new else yaml_data.get(ocboot.GROUP_PRIMARY_MASTER_NODE, {}).get('db_password')
