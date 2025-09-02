df0703a4f67d58baeac63d60b5b5be7a260176fb
Qiu Jian <qiujian@yunionyun.com>
2025-09-02 14:52:38 +0800

    fix: minor fixes

--- lib/add_node.py
+++ lib/add_node.py
@@ -3,10 +3,26 @@ from __future__ import unicode_literals
 
 import socket
 
+from . import consts
 from .service import AddNodeService, AddNodesConfig
 from .cluster import construct_cluster
 from .parser import inject_add_nodes_runtime_options
-from .ansible import parse_inventory_config
+
+
+def is_ipv4(addr):
+    try:
+        socket.inet_pton(socket.AF_INET, addr)
+        return True
+    except (OSError, AttributeError, socket.error):
+        return False
+
+
+def is_ipv6(addr):
+    try:
+        socket.inet_pton(socket.AF_INET6, addr)
+        return True
+    except (OSError, AttributeError, socket.error):
+        return False
 
 
 class AddWorkerNodeService(AddNodeService):
@@ -22,9 +38,13 @@ class AddWorkerNodeService(AddNodeService):
             args.ssh_private_file,
             args.ssh_port)
 
-        master_inventory = None
-        if args.master_inventory:
-            master_inventory = parse_inventory_config(args.master_inventory)
+        if args.ip_type == '':
+            if is_ipv4(args.primary_master_host):
+                args.ip_type = consts.IP_TYPE_IPV4
+            elif is_ipv6(args.primary_master_host):
+                args.ip_type = consts.IP_TYPE_IPV6
+            else:
+                raise ValueError("ip type is not set and cannot be determined from primary master host")
 
         config = AddNodesConfig(cluster,
                                 args.target_node_hosts,
@@ -37,7 +57,7 @@ class AddWorkerNodeService(AddNodeService):
                                 host_networks=args.host_networks,
                                 disk_paths=args.disk_paths,
                                 ip_dual_conf=getattr(args, 'ip_dual_conf', None),
-                                master_inventory=master_inventory,
+                                ip_type=args.ip_type,
                                 offline_data_path=args.offline_data_path,
                                 )
         config.run()
