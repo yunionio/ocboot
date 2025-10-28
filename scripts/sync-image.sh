4d04490896f308e6c7697aabc494dfc4bbfd99e0
Zexi Li <zexi.li@icloud.com>
2025-10-28 11:28:15 +0800

    fix: use calico v3.27.5

--- scripts/sync-image.sh
+++ scripts/sync-image.sh
@@ -1,16 +1,42 @@
 #!/bin/bash
 
-TARGET_REGISTRY=registry.cn-beijing.aliyuncs.com/yunion
+# Version configuration (can be overridden by environment variables)
+TARGET_REGISTRY=${TARGET_REGISTRY:-"registry.cn-beijing.aliyuncs.com/yunion"}
+CALICO_VERSION=${CALICO_VERSION:-"v3.27.5"}
 
 # skopeo login --username $USERNAME $TARGET_REGISTRY
 
 SKOPEO_COPY_CMD='skopeo copy --override-os linux --multi-arch all'
 
-# calico
-$SKOPEO_COPY_CMD docker://docker.io/calico/cni:v3.27.0 docker://$TARGET_REGISTRY/calico-cni:v3.27.0
-$SKOPEO_COPY_CMD docker://docker.io/calico/node:v3.27.0 docker://$TARGET_REGISTRY/calico-node:v3.27.0
-$SKOPEO_COPY_CMD docker://docker.io/calico/kube-controllers:v3.27.0 docker://$TARGET_REGISTRY/calico-kube-controllers:v3.27.0
+# Function: Copy image from source to target registry
+copy_image() {
+    local source_image=$1
+    local target_name=$2
+    local target_image="${TARGET_REGISTRY}/${target_name}"
+    
+    echo "Copying ${source_image} to ${target_image}..."
+    $SKOPEO_COPY_CMD docker://${source_image} docker://${target_image}
+}
 
-### k3s ###
-$SKOPEO_COPY_CMD docker://docker.io/rancher/mirrored-library-traefik:2.10.5 docker://$TARGET_REGISTRY/traefik:2.10.5
-$SKOPEO_COPY_CMD docker://docker.io/rancher/mirrored-coredns-coredns:1.10.1 docker://$TARGET_REGISTRY/coredns:1.10.1
+# Function: Add calico images to the copy list
+add_calico_images() {
+    local version=$1
+    IMAGES["calico-cni:${version}"]="docker.io/calico/cni:${version}"
+    IMAGES["calico-node:${version}"]="docker.io/calico/node:${version}"
+    IMAGES["calico-kube-controllers:${version}"]="docker.io/calico/kube-controllers:${version}"
+}
+
+# Define images to be copied
+declare -A IMAGES=()
+
+# Add calico images
+add_calico_images "${CALICO_VERSION}"
+
+# Add other images
+IMAGES["traefik:2.10.5"]="docker.io/rancher/mirrored-library-traefik:2.10.5"
+IMAGES["coredns:1.10.1"]="docker.io/rancher/mirrored-coredns-coredns:1.10.1"
+
+# Copy all images
+for target_name in "${!IMAGES[@]}"; do
+    copy_image "${IMAGES[$target_name]}" "${target_name}"
+done
